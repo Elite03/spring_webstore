745aaea40bec533f941dd7a5b2888a4afcfdecea
Elite03 <ppathania67@gmail.com>
2016-11-16 16:13:51 +0530

    Change-Id: Ic82275cfb130b73331398defdd4538ac5d8d3af7

---
+++ src/main/java/com/java/local/main/classic/spring/validator/UnitsInStockValidator.java
@@ -0,0 +1,28 @@
+package com.java.local.main.classic.spring.validator;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.java.local.main.model.domain.Product;
+
+@Component
+public class UnitsInStockValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Product.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object object, Errors errors) {
+		Product product = (Product) object;
+		if (product.getUnitPrice() != null && new BigDecimal(10000).compareTo(product.getUnitPrice()) <= 0
+				&& product.getUnitsInStock() > 99) {
+			errors.rejectValue("unitsInStock", "Spring.Validations.Classic.Spring.Validation");
+		}
+	}
+
+}
